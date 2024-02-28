e2a133d8ef7080adab376a1651eab333934400c4
SebasPCDev <sebpa.16@gmail.com>
2024-02-28 17:39:15 -0500

    modificacion

--- demo.js
+++ demo.js
@@ -1,2 +1,15 @@
 
 
+
+function sumar(a, b) {
+    return a + b
+}
+function restar(a, b) {
+    return a - b
+}
+function mult(a, b) {
+    return a * b
+}
+function divide(a, b) {
+    return a % b
+}
